e334d12d72a9bd4869d22fccd50db1728bbe9fe3
SpringDream0406 <SpringDream0406@gmail.com>
2023-12-18 18:39:30 +0900

    희준씨 껄로 변경

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import { Button, ButtonGroup } from 'react-bootstrap';
-import MainPage from './pages/MainPage';
-import AboutPage from './pages/AboutPage';
-import NoutFoundPage from './pages/NoutFoundPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom';
 
+import Header from './components/Header';
+import Home from "./components/Home";
+import TravelMate from './components/TravelMate';
+import Senimo from './components/Senimo';
 
 function App() {
-
-  const nav = useNavigate()
-  
-  
-
-
   return (
-    <div>
-        <div>
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="warning" onClick={()=>{nav('/')}}>Main</Button>
-            <Button variant="outline-warning" onClick={()=>{nav('/about')}}>About</Button>
-          </ButtonGroup>
-        </div>
-        
-        <Routes>
-          <Route path='/' element={<MainPage />}></Route>
-          <Route path='/about' element={<AboutPage />}></Route>
-
-          <Route path='*' element={<NoutFoundPage />}></Route>
-
-        </Routes>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tm" element={<TravelMate />}></Route>
+        <Route path="/senimo" element={<Senimo />}></Route>
+      </Routes>
     </div>
   );
 }
