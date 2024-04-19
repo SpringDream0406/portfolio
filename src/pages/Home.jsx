569987a2a1f3ce7e1551093a4f26d6fb6594593e
SpringDream0406 <springdream0406@gmail.com>
2024-04-20 01:50:01 +0900

    사진첩 제거

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-import Introduction from '../components/Introduce'
-import MyInfo from '../components/MyInfo'
-import Skill from '../components/Skill'
-import Experience from '../components/Experience'
-import Picture from '../components/Picture'
-import Certification from '../components/Certification'
-import Education from '../components/Education'
+import React from "react";
+import styled from "styled-components";
+import Introduction from "../components/Introduce";
+import MyInfo from "../components/MyInfo";
+import Skill from "../components/Skill";
+import Experience from "../components/Experience";
+import Certification from "../components/Certification";
+import Education from "../components/Education";
 
 const Home = () => {
   return (
@@ -23,14 +22,13 @@ const Home = () => {
       <DivLine />
       <Experience />
       <DivLine />
-      <Picture />
       <br />
-      <h2 className='tytext'>Thank you!</h2>
+      <h2 className="tytext">Thank you!</h2>
     </HomeField>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const HomeField = styled.div`
   display: flex;
@@ -41,18 +39,18 @@ const HomeField = styled.div`
   padding-top: 85px;
   padding-bottom: 85px;
   margin: auto;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     width: 80%;
   }
-  
-  .tytext{
+
+  .tytext {
     display: flex;
     justify-content: center;
   }
-`
+`;
 
 const DivLine = styled.div`
   margin: 50px 0;
   height: 1px;
   background-color: lightgray;
-`
+`;
